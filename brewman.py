cdb26fbbf68761d2e93e9cf32ee9de9e96b00e75
Adam Hoover <ashoover@gmail.com>
2017-08-14 00:13:55 -0500

    Night;y Push
    Moved 99% of print statements over to logging.
    Added a 'Heat' function.

--- brewman.py
+++ brewman.py
@@ -4,12 +4,6 @@ import time
 import logging
 import RPi.GPIO as GPIO
 
-
-# Setups
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-
 # Variables
 base_dir = '/sys/bus/w1/devices/'
 
@@ -17,25 +11,27 @@ base_dir = '/sys/bus/w1/devices/'
 # Settings
 sleep_time = 5 #300
 diag_mode = True
+logging_level = 'logging.INFO'
+log_file_name = 'app_logs.txt'
 relay_pin_1 = 12
-relay_pin_2 = 12
-log_weather = 'Y'
+relay_pin_2 = 13
+log_temperature = 'Y'
 temp_format = 'F'
 logs_folder = 'logs/'
 th_high = 80
 th_low = 70
+cd_th = 3
+wu_th = 3
 device_list = {'28-0516a1a7d7ff':'Sensor 1',
      	       '28-0416b059cfff':'Sensor 2'}
 
+# Setups
+logging.basicConfig(filename=log_file_name, level=logging_level, format="%(asctime)s %(levelname)s %(message)s")
+logging.info("Logging Enabled.")
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
 # Functions
-
-# Temp functions to be removed later #########
-def space():
-    space = '\n' + 32 * '-' + '\n'
-    print(space)
-
-##############################################
 
 def device_check():
     for device_id, device_name in device_list.items():
@@ -55,7 +51,6 @@ def device_check():
             lines = read_temp_raw()
 
             while lines[0].strip()[-3:] != 'YES':
-                #time.sleep(0.2)
                 lines = read_temp_raw()
             
             equals_pos = lines[1].find('t=')
@@ -66,74 +61,98 @@ def device_check():
                 temp_f = temp_c * 9.0 / 5.0 + 32.0
                 return temp_c, temp_f
 
+        def sensor():
+            if device_name.upper() == 'SENSOR 1':
+                relay_pin = relay_pin_1
+
+            elif device_name.upper() == 'SENSOR 2':
+                relay_pin = relay_pin_2
+
+            return relay_pin
+        relay_pin = sensor()
+
         # Fan Controller
         def fan_control(status):
 
-            if diag_mode == True :
-                print("Fan Control function Called with {} sent to it.".format(status))
-
-            def GPIOsetup():
+            def gpio_setup():
                 GPIO.setwarnings(False) 
                 GPIO.setmode(GPIO.BCM)
                 GPIO.setup(relay_pin_1, GPIO.OUT)
+                GPIO.setup(relay_pin_2, GPIO.OUT)
 
-            def sensor():
-                if device_name.upper() in 'SENSOR 1':
-                    relay_pin = relay_pin_1
-                    print("Using Relay 1")
-                    return relay_pin
+            print("Checking Device : {}".format(device_name))
 
+            if diag_mode:
 
-                elif device_name.upper() in 'SENSOR 2':
-                    relay_pin = relay_pin_2
-                    print("Using Relay 2")
-                    return relay_pin
-
-
-            relay_pin = sensor()
-                
             def fan_on():
-                GPIOsetup()
+                gpio_setup()
                 GPIO.output(relay_pin, 0)	#fan on
                 return()
-                print("Fan Turned ON for {}".format(device_name))
 
             def fan_off():
-                GPIOsetup()
+                gpio_setup()
                 GPIO.output(relay_pin, 1)	#fan off
                 return()
-                print("Fan Turned OFF for {}".format(device_name))
 
             if status.upper() == 'ON':
                 fan_on()
+
             elif status.upper() == 'OFF':
                 fan_off()
+
+            elif status.upper() == 'NONE':
+                fan_off()
+
+            else:
+                logging.error('No status for {} found.'.format(status.upper()))
+
+        # Heat Control
+        def heat_control(status):
+            def heat_on():
+                print("Heat control ON.")
+
+            def heat_off():
+                print("Heat control off.")
+
+            if status.upper() == 'ON':
+                heat_on()
+
+            elif status.upper() == 'OFF':
+                heat_off()
+
+            elif status.upper() == 'NONE':
+                heat_off()
         
         # Action to take based on current temperature
         def temp_action():
-
-            if ctemp_f <= int(th_low):
-                att = "OFF"
-                fan_control(att)
-                print()
-                return att
-
-            elif ctemp_f >= int(th_high):
+            if ctemp_f >= th_high:
                 att = "ON"
                 fan_control(att)
-                return att
 
-            print("Temp action called and returned {}".format(att))
-        
-        # Write to sensor titled logs in the 'logs' folder
+            elif ctemp_f <= (th_high - cd_th): # cool-down temp
+                att = "OFF"
+                fan_control(att)
+
+            elif ctemp_f < (th_low - wu_th):
+                att = "ON"
+                heat_control(att)
+
+            else:
+                att = "NONE"
+                fan_control(att)
+                heat_control(att)
+
+            return att
+
+        # Write to sensor ID titled logs in the 'logs' folder
         def write_to_log():
             log_data = [device_name, device_id, ctemp_f, ctemp_c, current_time, actiontt]
 
-            print("Logging : {} \n".format(log_data))
+            temp_data_exist = os.path.exists(temp_data_file)
 
-            if os.path.exists(temp_data_file) == False:
+            if temp_data_exist is False:
                 open(temp_data_file, 'a').close()
-                print("Log file {} has been Created ...".format(temp_data_file))
+                logging.info("Log file {} has been Created ...".format(temp_data_file))
 
             with open(temp_data_file, mode='+a', encoding='utf-8') as log_open:
                 final_write_data = str(log_data) + '\n'
@@ -146,28 +165,16 @@ def device_check():
         ctemp_c = int(current_temp[0])
         actiontt = temp_action()
 
-
-## Delete me later ##
-        print(actiontt)
-        print(type(actiontt))
-
-#####################
-
-        print('Checking {}'.format(device_name))
-        print('Sensor file location: {}'.format(device_id_loc))
-        print('Action : {}'.format(actiontt))
-
         if temp_format.upper() == 'F':
             print("Current temp on {} is : {}F at {}".format(device_name, ctemp_f, current_time))
         else:
             print("Current temp on {} is : {}C at {}".format(device_name, ctemp_c, current_time))
      
-        if log_weather.upper() == 'Y':
+        if log_temperature.upper() == 'Y':
             write_to_log()
 
 
 # App Call
 while True:
-    space() # Will be removed later
     device_check()
     time.sleep(sleep_time)
